2bcedc9553f0d33dd0a9d68de556d9d64e05fbbd
Greyson Wright <greysonmwright@gmail.com>
2015-09-30 09:31:19 -0500

    - displays wislr and stn ids instead of rows so that the errors can actually be found - optimizations

--- Date Check Tool/Date Check Tool/MainWindow.xaml.cs
+++ Date Check Tool/Date Check Tool/MainWindow.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Collections.Generic;
+﻿//Created by Greyson Wright 2015
+//This is just a great way to make sure the dates are right I guess. If you happen to use DataBaseTools don't get it from this proj because its outdated. The one in date pop is way better sort of. 
+//Be sure to read my intro comments to date pop if you havn't already done so. It has great info in it.
+
+using System.Collections.Generic;
 using System.Windows;
 using Microsoft.Win32;
 using System.ComponentModel;
@@ -13,7 +17,8 @@ namespace Date_Check_Tool
     public partial class MainWindow : Window
     {
         
-        BackgroundWorker backgroundWorker;
+        //Global variables bother me
+        BackgroundWorker backgroundWorker; 
         string currentProcess;
         string filePath;
         string tableName;
@@ -25,11 +30,13 @@ namespace Date_Check_Tool
         {
             InitializeComponent();
 
+            //Displays select table in combo box
             tableComboBox.Items.Add("Select Table...");
             tableComboBox.Text = "Select Table...";
 
         }
 
+        //A method that returns an array that will later be written to a text file each element is a line of a text file
         private string[] getBadDates()
         {
 
@@ -40,19 +47,29 @@ namespace Date_Check_Tool
             dbTools.progressUpdated += dbTools_ProgressUpdated;
             dbTools.didReceiveFatalError += dbTools_didReceiveFatalError;
             table = dbTools.getDataTable(filePath, tableName);
-            //13 rc
+
+            string stnId;
+            string wislrId;
+            string recordCreated;
+            string recordHistoric;
+            string startValid;
+            string endValid;
+            int localErrorCount; //Local errorCount keeps up with errors per row while errorCount is total erros over the whole file the error count is displayed once date check is done
+
             for (int i = 0; i < table.Rows.Count; i++)
             {
-                
-                string recordCreated = table.Rows[i].ItemArray[12].ToString();
-                string recordHistoric = table.Rows[i].ItemArray[13].ToString();
-                string startValid = table.Rows[i].ItemArray[14].ToString();
-                string endValid = table.Rows[i].ItemArray[15].ToString();
-                int localErrorCount = 0;
 
-                writeContents.Add("Row " + (i + 1) + " {");
+                stnId = table.Rows[i].ItemArray[0].ToString();
+                wislrId = table.Rows[i].ItemArray[3].ToString();   
+                recordCreated = table.Rows[i].ItemArray[12].ToString();
+                recordHistoric = table.Rows[i].ItemArray[13].ToString();
+                startValid = table.Rows[i].ItemArray[14].ToString();
+                endValid = table.Rows[i].ItemArray[15].ToString();
+                localErrorCount = 0;  
 
-                if (string.IsNullOrEmpty(recordHistoric) && !string.IsNullOrEmpty(endValid))
+                writeContents.Add("STNID: " + stnId +" WISLRID: " + wislrId + " {"); //The rows aren't always the same in our datatables and the datatable in access so we give them the stnid and wislrid
+
+                if (string.IsNullOrEmpty(recordHistoric) && !string.IsNullOrEmpty(endValid)) //No end valid date for record historic
                 {
                     
                     writeContents.Add("    There is no Record Historic for End Valid");
@@ -64,13 +81,13 @@ namespace Date_Check_Tool
                 if (DateTools.getLaterDate(endValid, startValid) == startValid)
                 {
                                         
-                    writeContents.Add("    Start Valid is more recent than End Valid");
+                    writeContents.Add("    Start Valid is more recent than End Valid"); //End valid is earlier than the start valid
                     errorCount++;
                     localErrorCount++;
 
                 }
 
-                if (DateTools.getLaterDate(recordHistoric, startValid) == startValid)
+                if (DateTools.getLaterDate(recordHistoric, startValid) == startValid) //Start valid is more recent than record historic
                 {
 
                     writeContents.Add("    Start Valid is more recent than Record Historic");
@@ -79,7 +96,7 @@ namespace Date_Check_Tool
 
                 }
 
-                if (DateTools.getLaterDate(recordCreated, startValid) == startValid)
+                if (DateTools.getLaterDate(recordCreated, startValid) == startValid) //Start valid is more recent than record created
                 {
 
                     writeContents.Add("    Start Valid is more recent than Record Created");
@@ -88,22 +105,22 @@ namespace Date_Check_Tool
 
                 }
 
-                if (localErrorCount == 0)
+                if (localErrorCount == 0) //If we don't have errors here remove the opening line and brace
                 {
 
                     writeContents.RemoveAt(writeContents.Count - 1);
 
                 }
-                else
+                else //If we have local errors close it off with a brace and a new line
                 {
 
-                    writeContents.Add("}\n");
+                    writeContents.Add("}\n"); 
 
                 }
 
             }
 
-            return writeContents.ToArray();
+            return writeContents.ToArray(); //Return array of lines
 
         }
 
@@ -137,12 +154,12 @@ namespace Date_Check_Tool
         private void goButton_Click(object sender, RoutedEventArgs e)
         {
 
-            if (filePath != null && tableComboBox.Text != "Select table...")
+            if (filePath != null && tableComboBox.Text != "Select table...") //Check if user has selected a table
             {
 
-                shouldClose = false;
-                tableName = tableComboBox.Text;
-                backgroundWorker = new BackgroundWorker();
+                shouldClose = false; //Don't let user close us until we finish
+                tableName = tableComboBox.Text; //Get the tablename from the combo box
+                backgroundWorker = new BackgroundWorker(); //Initialize a background worker
                 backgroundWorker.WorkerReportsProgress = true;
                 backgroundWorker.WorkerSupportsCancellation = true;
                 backgroundWorker.DoWork += backgroundWorker_DoWork;
@@ -165,15 +182,15 @@ namespace Date_Check_Tool
         {
 
             string[] writeContents = getBadDates();
-            using (StreamWriter streamWriter = new StreamWriter("output.txt"))
+            if (errorCount > 0) //Don't worry about writing if there are no errors
             {
-                if (errorCount > 0)
+                using (StreamWriter streamWriter = new StreamWriter("output.txt"))
                 {
 
-                    for (int i = 0; i < writeContents.Length; i++)
+                    for (int i = 0; i < writeContents.Length; i++) //Iterate over the array
                     {
 
-                        streamWriter.WriteLine(writeContents[i]);
+                        streamWriter.WriteLine(writeContents[i]); //Write each element in the array to the file
 
                     }
 
@@ -189,10 +206,10 @@ namespace Date_Check_Tool
             shouldClose = true; //The user now has permission to kill date pop so don't get mad
             Dispatcher.Invoke(() => { //We have to invoke this other stuff because its crossthreaded
 
-                if (errorCount > 0)
-                    System.Diagnostics.Process.Start("notepad.exe", "output.txt"); //Opens the text file in notepad
-
                 MessageBox.Show("The Date Check Tool finished and found " + errorCount + " errors.");
+
+                if (errorCount > 0) //Don't worrry about opening the file unless we have errors
+                    System.Diagnostics.Process.Start("notepad.exe", "output.txt"); //Opens the text file in notepad
 
             });
 
